5ece50605c5c91c476ad62e3ced1c858c2dd2142
Enrique Mendoza <enriquemc250199@gmail.com>
2024-12-07 10:23:09 -0700

    Project 5

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,3 +1,4 @@
+// File: Agent/Agent.java
 package Agent;
 
 import constants.Message;
@@ -7,16 +8,12 @@ import java.net.*;
 import java.util.*;
 
 public class Agent {
-    private Socket bankSocket;
-    private HashMap<String, Socket> auctionSockets;
-    private ObjectOutputStream bankOut;
-    private ObjectInputStream bankIn;
+    private BankConnection bankConnection;
+    private AuctionManager auctionManager;
     private int accountNum;
     private Scanner scanner;
     private boolean isAuto;
     private boolean running;
-    private BankConnection bankConnection;
-    private AuctionManager auctionManager;
 
     public static void main(String[] args) {
         if (args.length < 3) {
@@ -39,26 +36,22 @@ public class Agent {
     public Agent(boolean isAuto) {
         System.out.println("Initializing Agent...");
         this.isAuto = isAuto;
-        this.auctionSockets = new HashMap<>();
-        this.listeners = new HashMap<>();
         this.scanner = new Scanner(System.in);
         this.running = true;
+        this.auctionManager = new AuctionManager();
     }
 
     public void start(int bankPort, String name, int initialBalance) throws IOException, ClassNotFoundException {
         try {
             System.out.println("Connecting to bank at localhost:" + bankPort);
-            bankSocket = new Socket("localhost", bankPort);
+            bankConnection = new BankConnection(bankPort);
             System.out.println("Connected to bank");
 
-            bankOut = new ObjectOutputStream(bankSocket.getOutputStream());
-            bankIn = new ObjectInputStream(bankSocket.getInputStream());
-
             System.out.println("Registering with bank...");
-            bankOut.writeObject(new Message("NewAgent"));
-            bankOut.writeObject(new Message("CreateNewAccount", initialBalance, name));
+            bankConnection.sendMessage(new Message("NewAgent"));
+            bankConnection.sendMessage(new Message("CreateNewAccount", initialBalance, name));
 
-            Message response = (Message) bankIn.readObject();
+            Message response = bankConnection.receiveMessage();
             accountNum = (Integer) response.splitCommand(1);
             System.out.println("Account created with number: " + accountNum);
 
@@ -87,11 +80,9 @@ public class Agent {
 
                 int choice = scanner.nextInt();
                 scanner.nextLine(); // consume newline
-
                 handleMenuChoice(choice);
             } catch (Exception e) {
-                System.out.println("Error processing menu choice:");
-                e.printStackTrace();
+                System.out.println("Error in menu: " + e.getMessage());
             }
         }
     }
@@ -105,7 +96,7 @@ public class Agent {
                 viewAuctionHouses();
                 break;
             case 3:
-                viewItems();
+                auctionManager.viewItems();
                 break;
             case 4:
                 placeBid();
@@ -120,15 +111,15 @@ public class Agent {
 
     private void checkBalance() throws IOException, ClassNotFoundException {
         System.out.println("Checking balance...");
-        bankOut.writeObject(new Message("availableBalance"));
-        Message response = (Message) bankIn.readObject();
+        bankConnection.sendMessage(new Message("availableBalance"));
+        Message response = bankConnection.receiveMessage();
         System.out.println("Available Balance: $" + response.splitCommand(1));
     }
 
     private void viewAuctionHouses() throws IOException, ClassNotFoundException {
         System.out.println("Requesting auction house list...");
-        bankOut.writeObject(new Message("ViewCurrentAuctions"));
-        Message response = (Message) bankIn.readObject();
+        bankConnection.sendMessage(new Message("ViewCurrentAuctions"));
+        Message response = bankConnection.receiveMessage();
         HashMap<?, AuctionHouseAddress> auctions =
                 (HashMap<?, AuctionHouseAddress>) response.splitCommand(1);
 
@@ -142,48 +133,18 @@ public class Agent {
             AuctionHouseAddress addr = entry.getValue();
             String addressKey = addr.getIpAddress() + ":" + addr.getPortNum();
             System.out.println(addressKey);
-            connectToAuctionHouse(addr);
-        }
-    }
-
-    private void connectToAuctionHouse(AuctionHouseAddress address) throws IOException {
-        String addressKey = address.getIpAddress() + ":" + address.getPortNum();
-        if (!auctionSockets.containsKey(addressKey)) {
-            System.out.println("Connecting to auction house at " + addressKey);
-            Socket socket = new Socket(address.getIpAddress(), address.getPortNum());
-            auctionSockets.put(addressKey, socket);
-            AuctionListener listener = new AuctionListener(socket);
-            listeners.put(socket, listener);
-            new Thread(listener).start();
-            System.out.println("Connected to auction house at " + addressKey);
-        }
-    }
-
-    private void viewItems() throws IOException {
-        if (auctionSockets.isEmpty()) {
-            System.out.println("No auction houses connected. Please view auction houses first.");
-            return;
-        }
-
-        System.out.println("Requesting items from all auction houses...");
-        for (Map.Entry<String, Socket> entry : auctionSockets.entrySet()) {
-            try {
-                ObjectOutputStream out = new ObjectOutputStream(entry.getValue().getOutputStream());
-                out.writeObject(new Message("GetItems"));
-            } catch (IOException e) {
-                System.out.println("Error requesting items from " + entry.getKey());
-            }
+            auctionManager.connectToAuctionHouse(addr);
         }
     }
 
     private void placeBid() throws IOException {
-        if (auctionSockets.isEmpty()) {
-            System.out.println("No auction houses connected. Please view auction houses first.");
+        if (auctionManager.getAuctionAddresses().isEmpty()) {
+            System.out.println("No auction houses connected. View auction houses first.");
             return;
         }
 
         System.out.println("\nAvailable auction houses:");
-        for (String address : auctionSockets.keySet()) {
+        for (String address : auctionManager.getAuctionAddresses()) {
             System.out.println(address);
         }
 
@@ -194,29 +155,16 @@ public class Agent {
         System.out.print("Enter bid amount: $");
         int bidAmount = scanner.nextInt();
 
-        Socket socket = auctionSockets.get(auctionAddress);
-        if (socket != null && socket.isConnected()) {
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            out.writeObject(new Message("Bid", itemId, accountNum, bidAmount));
-            System.out.println("Bid sent. Awaiting response...");
-        } else {
-            System.out.println("Invalid auction house address or connection lost");
-        }
+        auctionManager.placeBid(auctionAddress, itemId, accountNum, bidAmount);
     }
 
     private void exit() throws IOException {
         System.out.println("Exiting...");
         running = false;
 
-        // Close bank connection
-        bankOut.writeObject(new Message("Terminates"));
-        bankSocket.close();
-
-        // Close auction house connections
-        for (Map.Entry<Socket, AuctionListener> entry : listeners.entrySet()) {
-            entry.getValue().stop();
-            entry.getKey().close();
-        }
+        bankConnection.sendMessage(new Message("Terminates"));
+        bankConnection.close();
+        auctionManager.closeAll();
 
         System.out.println("Goodbye!");
         System.exit(0);
@@ -230,7 +178,7 @@ public class Agent {
             try {
                 Thread.sleep(5000);
                 viewAuctionHouses();
-                viewItems();
+                auctionManager.viewItems();
                 // Auto-bidding logic could be implemented here
             } catch (Exception e) {
                 if (running) {
@@ -240,29 +188,8 @@ public class Agent {
             }
         }
     }
-    public class BankConnection {
-        private Socket socket;
-        private ObjectOutputStream out;
-        private ObjectInputStream in;
 
-        public BankConnection(Socket socket) throws IOException {
-            this.socket = socket;
-            this.out = new ObjectOutputStream(socket.getOutputStream());
-            this.in = new ObjectInputStream(socket.getInputStream());
-        }
-
-        public void sendMessage(Message message) throws IOException {
-            out.writeObject(message);
-        }
-
-        public Message receiveMessage() throws IOException, ClassNotFoundException {
-            return (Message) in.readObject();
-        }
-
-        public void close() throws IOException {
-            socket.close();
-        }
-    }
+    // Getters for other classes to use
     public boolean isRunning() {
         return running;
     }
--- src/Agent/BankConnection.java
+++ src/Agent/BankConnection.java
@@ -5,65 +5,27 @@ import java.io.*;
 import java.net.*;
 
 public class BankConnection {
-    private Socket bankSocket;
-    private ObjectOutputStream bankOut;
-    private ObjectInputStream bankIn;
-    private Agent agent;
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
 
-    public BankConnection(int bankPort, Agent agent) throws IOException {
-        this.agent = agent;
-        connect(bankPort);
-    }
-
-    private void connect(int bankPort) throws IOException {
-        bankSocket = new Socket("localhost", bankPort);
-        bankOut = new ObjectOutputStream(bankSocket.getOutputStream());
-        bankIn = new ObjectInputStream(bankSocket.getInputStream());
-        new Thread(new BankListener()).start();
-    }
-
-    public int register(String name, int initialBalance) throws IOException, ClassNotFoundException {
-        bankOut.writeObject(new Message("NewAgent"));
-        bankOut.writeObject(new Message("CreateNewAccount", initialBalance, name));
-        Message response = (Message) bankIn.readObject();
-        return (Integer) response.splitCommand(1);
+    public BankConnection(int bankPort) throws IOException {
+        this.socket = new Socket("localhost", bankPort);
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
     }
 
     public void sendMessage(Message message) throws IOException {
-        bankOut.writeObject(message);
+        out.writeObject(message);
     }
 
     public Message receiveMessage() throws IOException, ClassNotFoundException {
-        return (Message) bankIn.readObject();
+        return (Message) in.readObject();
     }
 
     public void close() throws IOException {
-        bankOut.writeObject(new Message("Terminates"));
-        bankSocket.close();
-    }
-
-    private class BankListener implements Runnable {
-        @Override
-        public void run() {
-            while (agent.isRunning()) {
-                try {
-                    Message message = (Message) bankIn.readObject();
-                    handleBankMessage(message);
-                } catch (Exception e) {
-                    if (agent.isRunning()) e.printStackTrace();
-                }
-            }
-        }
-
-        private void handleBankMessage(Message message) {
-            switch (message.getCommand()) {
-                case "winner":
-                    System.out.println("Won auction for item: " + message.splitCommand(1));
-                    break;
-                case "outbid":
-                    System.out.println("Outbid on item: " + message.splitCommand(1));
-                    break;
-            }
+        if (socket != null) {
+            socket.close();
         }
     }
 }
